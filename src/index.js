d79e661abd778fb90226931450967a3f10ca4092
+ <ronoh.ivy@gmail.com>
2022-06-15 03:56:01 +0300

    Delete Function Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit' ,(e) => {
+    e.preventDefault()
+    newTask(e.target.new_task_description.value)
+    form.reset()
+  })
+  
 });
+
+function newTask(task) {
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click' , deleteTask)
+  btn.textContent = 'Remove Item'
+  li.textContent = `${task} `
+  li.appendChild(btn)
+  document.getElementById("tasks").appendChild(li)
+}
+
+function deleteTask(e){
+  e.target.parentNode.remove()
+}
+
